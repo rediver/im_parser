51a84f13fa487bc1d091d80de6bd154312bc7213
Your Name <you@example.com>
2020-12-11 13:18:36 +0100

    mod

--- main.py
+++ main.py
@@ -1,52 +1,34 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import time
+import requests
 import pandas as pd
+from bs4 import BeautifulSoup
 
-link = "https://wyniki.datasport.pl/results3029/"
-
-driver = webdriver.Firefox()
-driver.get(link)
-
-
-lastHeight = driver.execute_script("return document.body.scrollHeight")
-#print(lastHeight)
-
-pause = 0.5
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(pause)
-    newHeight = driver.execute_script("return document.body.scrollHeight")
-    if newHeight == lastHeight:
-        break
-    lastHeight = newHeight
-    #print(lastHeight)
-
-html = driver.page_source
-soup = BeautifulSoup(html, "lxml")
-
-
-# array
 appended_data = []
-table_body = soup.find('tbody')
-# print(table_body)
-rows = table_body.find_all('tr')
-for row in rows:
-    cols=row.find_all('td')
-    cols=[x.text.strip() for x in cols]
-    appended_data.append(cols)
+url = 'https://www.multisportaustralia.com.au/races/IM703-TAUPO-2019/events/1?page='   
+links = []
+for i in range(1,30):
+	x = url + str(i)
+	links.append(x)
+
+print(links)
+results = []
+
+for link in links:
+  webpage = requests.get(link)
+  data = webpage.text 
+  bs=BeautifulSoup(data, "lxml")
+  table_body=bs.find('tbody')
+  rows = table_body.find_all('tr')
+  for row in rows:
+    	cols=row.find_all('td')
+    	cols=[x.text.strip() for x in cols]
+    	results.append(cols)
 
 
-# print(appended_data)
-appended_data2 = pd.DataFrame.from_dict(appended_data)
+# print(results)
 
-appended_data2.columns = ['Pos', 'x',  'Name', 'Club' , 'Rocznik' , 'Cat' , 'Swim', 'T1', 'Bike', 'T2', 'Run', 'Time']
+appended_data2 = pd.DataFrame.from_dict(results)
+print(appended_data2)
 
-del appended_data2['x']
-del appended_data2['Rocznik']
-# del appended_data2.loc
 
-x = appended_data2.drop([appended_data2.index[0] , appended_data2.index[10]], inplace=True)
+appended_data2.to_csv("im.csv")
 
-appended_data2.to_csv('gdynia5.csv')
-print('Job finished!')
